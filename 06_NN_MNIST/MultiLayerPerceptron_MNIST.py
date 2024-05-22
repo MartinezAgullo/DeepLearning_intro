2ee860a83b58bea4e5ed8a829c3ae67111154fd5
MartinezAgullo <pablo.martinez.agullo@cern.ch>
2024-05-22 16:41:18 +0200

    Add training loop

--- 06_NN_MNIST/MultiLayerPerceptron_MNIST.py
+++ 06_NN_MNIST/MultiLayerPerceptron_MNIST.py
@@ -1,21 +1,21 @@
 import torch
 import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
 
 from six.moves import urllib
 opener = urllib.request.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 urllib.request.install_opener(opener)
 
-from torchvision import datasets
-import torchvision.transforms as transforms
 
-# number of subprocesses to use for data loading
-num_workers = 0
-# how many samples per batch to load
-batch_size = 20
-
-# convert data to torch.FloatTensor
-transform = transforms.ToTensor()
+num_workers = 0                     # number of subprocesses to use for data loading
+batch_size = 20                     # how many samples per batch to load
+transform = transforms.ToTensor()   # convert data to torch.FloatTensor
 
 # choose the training and test datasets
 train_data = datasets.MNIST(root='data', train=True,
@@ -30,18 +30,90 @@ test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size,
     num_workers=num_workers)
 
 
-import matplotlib.pyplot as plt
-    
-# obtain one batch of training images
-dataiter = iter(train_loader)
-images, labels = dataiter.next()
-images = images.numpy()
+if False:   
+    # obtain one batch of training images
+    dataiter = iter(train_loader)
+    images, labels = dataiter.next()
+    images = images.numpy()
 
-# plot the images in the batch, along with the corresponding labels
-fig = plt.figure(figsize=(25, 4))
-for idx in np.arange(20):
-    ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
-    ax.imshow(np.squeeze(images[idx]), cmap='gray')
-    # print out the correct label for each image
-    # .item() gets the value contained in a Tensor
-    ax.set_title(str(labels[idx].item()))
+    #plot the images in the batch, along with the corresponding labels
+    fig = plt.figure(figsize=(25, 4))
+    for idx in np.arange(20):
+        ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+        ax.imshow(np.squeeze(images[idx]), cmap='gray')
+        # print out the correct label for each image
+        # .item() gets the value contained in a Tensor
+        ax.set_title(str(labels[idx].item()))
+
+# define the NN architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # number of hidden nodes in each layer (512)
+        hidden_1 = 512
+        hidden_2 = 512
+        # linear layer (784 -> hidden_1)
+        self.fc1 = nn.Linear(28 * 28, hidden_1)
+        # linear layer (n_hidden -> hidden_2)
+        self.fc2 = nn.Linear(hidden_1, hidden_2)
+        # linear layer (n_hidden -> 10)
+        self.fc3 = nn.Linear(hidden_2, 10)
+        # dropout layer (p=0.2)
+        # dropout prevents overfitting of data
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+        # flatten image input
+        x = x.view(-1, 28 * 28)
+        # add hidden layer, with relu activation function
+        x = F.relu(self.fc1(x)) # The purpose of an activation function is to scale the outputs of a layer so that they are a consistent, small value.
+        # add dropout layer
+        x = self.dropout(x)
+        # add hidden layer, with relu activation function
+        x = F.relu(self.fc2(x))
+        # add dropout layer
+        x = self.dropout(x)
+        # add output layer
+        x = self.fc3(x)
+        return x
+
+
+model = Net() # initialize the NN
+print(model)
+criterion = nn.CrossEntropyLoss()                           # specify loss function (categorical cross-entropy)
+                                                            # CrossEntropyLoss combines LogSoftMax and NLLLos
+                                                            # Very useful for classification processes
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)    # specify optimizer (stochastic gradient descent) and learning rate = 0.01
+
+n_epochs = 50 # number of epochs to train the model
+model.train() # prep model for training
+
+for epoch in range(n_epochs):
+    # monitor training loss
+    train_loss = 0.0
+    
+    ###################
+    # train the model #
+    ###################
+    for data, target in train_loader: # batch loop
+        # clear the gradients of all optimized variables
+        optimizer.zero_grad()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the loss
+        loss = criterion(output, target)
+        # backward pass: compute gradient of the loss with respect to model parameters
+        loss.backward()
+        # perform a single optimization step (parameter update)
+        optimizer.step()
+        # update running training loss
+        train_loss += loss.item()*data.size(0)
+             
+    # print training statistics 
+    # calculate average loss over an epoch
+    train_loss = train_loss/len(train_loader.dataset)
+
+    print('Epoch: {} \tTraining Loss: {:.6f}'.format(
+        epoch+1, 
+        train_loss
+        ))
