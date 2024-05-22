9842920a77520762d108a170925b3427704c0f5f
MartinezAgullo <pablo.martinez.agullo@cern.ch>
2024-05-22 16:46:32 +0200

    Add test step

--- 06_NN_MNIST/MultiLayerPerceptron_MNIST.py
+++ 06_NN_MNIST/MultiLayerPerceptron_MNIST.py
@@ -80,6 +80,11 @@ class Net(nn.Module):
 
 model = Net() # initialize the NN
 print(model)
+
+
+########################
+#     TRAINING STEP    #
+########################
 criterion = nn.CrossEntropyLoss()                           # specify loss function (categorical cross-entropy)
                                                             # CrossEntropyLoss combines LogSoftMax and NLLLos
                                                             # Very useful for classification processes
@@ -107,7 +112,7 @@ for epoch in range(n_epochs):
         # perform a single optimization step (parameter update)
         optimizer.step()
         # update running training loss
-        train_loss += loss.item()*data.size(0)
+        train_loss += loss.item()*data.size(0) # data.size(0) is the size of the batch
              
     # print training statistics 
     # calculate average loss over an epoch
@@ -117,3 +122,47 @@ for epoch in range(n_epochs):
         epoch+1, 
         train_loss
         ))
+    
+
+######################
+#      TEST STEP     #
+######################
+    # initialize lists to monitor test loss and accuracy
+test_loss = 0.0
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+
+model.eval() # prep model for training
+
+for data, target in test_loader:
+    # forward pass: compute predicted outputs by passing inputs to the model
+    output = model(data)
+    # calculate the loss
+    loss = criterion(output, target)
+    # update test loss 
+    test_loss += loss.item()*data.size(0)
+    # convert output probabilities to predicted class
+    _, pred = torch.max(output, 1)
+    # compare predictions to true label
+    correct = np.squeeze(pred.eq(target.data.view_as(pred)))
+    # calculate test accuracy for each object class
+    for i in range(batch_size):
+        label = target.data[i]
+        class_correct[label] += correct[i].item()
+        class_total[label] += 1
+
+# calculate and print avg test loss
+test_loss = test_loss/len(test_loader.dataset)
+print('Test Loss: {:.6f}\n'.format(test_loss))
+
+for i in range(10):
+    if class_total[i] > 0:
+        print('Test Accuracy of %5s: %2d%% (%2d/%2d)' % (
+            str(i), 100 * class_correct[i] / class_total[i],
+            np.sum(class_correct[i]), np.sum(class_total[i])))
+    else:
+        print('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+
+print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
+    100. * np.sum(class_correct) / np.sum(class_total),
+    np.sum(class_correct), np.sum(class_total)))
